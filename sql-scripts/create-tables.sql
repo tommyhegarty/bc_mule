4d6795c638c2d7332b6cfd0c6e15d61232f64ddf
Tommy Hegarty <thomas.ps.hegarty@gmail.com>
2019-08-21 15:29:25 -0400

    added updated sql file

--- sql-scripts/create-tables.sql
+++ sql-scripts/create-tables.sql
@@ -1,18 +1,15 @@
 CREATE TABLE teams (
-    teamId int not null auto_increment,
     team varchar(30),
     tag varchar(3),
     region varchar(3),
     wins integer,
     losses integer,
-    primary key (teamId)
+    primary key (tag)
 );
 
 CREATE TABLE players (
-    playerId int not null auto_increment,
     ign varchar(30),
-    team varchar(30),
-    tag varchar(3),
+    team varchar(3),
     igrole varchar(3),
-    primary key (playerId)
+    primary key (ign)
 );
